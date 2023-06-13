cc2a22c90d044980c7e82ca7b6bec2245ff5d758
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2023-06-13 15:49:41 +0100

    pushing

--- src/main/java/com/labelvie/lablecious/backend/DemoApplication.java
+++ src/main/java/com/labelvie/lablecious/backend/DemoApplication.java
@@ -3,11 +3,14 @@ package com.labelvie.lablecious.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-}
 
+}
